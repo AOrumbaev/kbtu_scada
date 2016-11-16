fdfc4648ee2bb74327bb91f715f68c484f50944d
Altynbek Orumbayev <stay.in.there@gmail.com>
2016-11-16 18:34:16 +0600

    Initial Commit

---
+++ KBTU_SCADA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KBTU_SCADA
+//
+//  Created by Altynbek Orumbayev on 16/11/2016.
+//  Copyright © 2016 A_Orumbayev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KBTU_SCADA/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KBTU_SCADA
+//
+//  Created by Altynbek Orumbayev on 16/11/2016.
+//  Copyright © 2016 A_Orumbayev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
